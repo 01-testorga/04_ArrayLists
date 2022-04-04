6920e837de04ca887dbc943f3da05298ea268282
Randy Bilz <bilz.randy@gmail.com>
2022-04-04 08:21:34 +0000

    4/4/22 10:21

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,9 +1,32 @@
 package com.cc.java;
 
+import java.util.ArrayList;
+
 public class App {
     
     public static void main(String[] args) {
-     
+    
+     //   Order order = new Order("order_1");
+     //   System.out.println(order.getName());
+
+    ArrayList<Order> orders = new ArrayList<>();
+    /*
+    System.out.println("Orders: " + orders);
+    orders.add(new Order("auftrag_1"));  
+    System.out.println("Orders1: " + orders);
+    orders.add(new Order("auftrag_2"));  
+    System.out.println("Orders2: " + orders);
+    System.out.println(orders.get(0));
+    System.out.println(orders.get(orders.size()-1));
+    */
+
+    for (int i =0; i <4; i++){
+    orders.add(new Order("Auftrag_" + i));       
+    }
+    System.out.println("Orders: " + orders);
+    System.out.println(orders.get(0).getName());
+    System.out.println(orders.get(orders.size()-1));
+
     }
 
 }
---
+++ com/cc/java/Order.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+public class Order {
+    
+    public String name;
+    public Order (String name){
+    this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
+
